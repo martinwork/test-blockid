a5e10cd540970077b9f5918419c7ba7f7e98da89
Martin Williams <martinwork@users.noreply.github.com>
2024-08-30 16:42:58 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace custom {
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+    //% blockId="map"
+    //% block
+    export function increment(value: number): number {
+        return value + 1;
+    }
+}
